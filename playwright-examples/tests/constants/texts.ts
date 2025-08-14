ac9936859bee31218ac4123158da833c9a478696
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-14 16:55:42 +0700

    feat: create constants for texts

---
+++ playwright-examples/tests/constants/texts.ts
@@ -0,0 +1,41 @@
+export const TEXTS = {
+  TITLES: {
+    APP: 'Swag Labs',
+    PRODUCTS: 'Products',
+    YOUR_CART: 'Your Cart',
+    CHECKOUT_INFORMATION: 'Checkout: Your Information',
+    CHECKOUT_OVERVIEW: 'Checkout: Overview',
+    CHECKOUT_COMPLETE: 'Checkout: Complete!',
+  },
+
+  BUTTONS: {
+    ADD_TO_CART: 'Add to cart',
+    REMOVE: 'Remove',
+    CONTINUE: 'Continue',
+    CANCEL: 'Cancel',
+    FINISH: 'Finish',
+    BACK_HOME: 'Back Home',
+    CONTINUE_SHOPPING: 'Continue Shopping',
+    CHECKOUT: 'Checkout',
+  },
+
+  PRICES: {
+    BACKPACK: '$29.99',
+    BIKE_LIGHT: '$9.99',
+    T_SHIRT: '$15.99',
+  },
+
+  DESCRIPTIONS: {
+    BACKPACK:
+      'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
+    BIKE_LIGHT:
+      "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
+  },
+
+  MESSAGES: {
+    CHECKOUT_COMPLETE_HEADER: 'Thank you for your order!',
+    CHECKOUT_COMPLETE_TEXT:
+      'Your order has been dispatched, and will arrive just as fast as the pony can get there!',
+    ABOUT_PAGE_TITLE: /.*Sauce Labs: Cross Browser Testing.*/,
+  },
+};
