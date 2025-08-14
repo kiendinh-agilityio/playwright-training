7d82d310f0681a671e541c2bd218af9e442e5ef2
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-14 16:57:13 +0700

    feat: create constants for message

---
+++ playwright-examples/tests/constants/message.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+  LOGIN_MISMATCH: 'Epic sadface: Username and password do not match any user in this service',
+  LOGIN_USERNAME_REQUIRED: 'Epic sadface: Username is required',
+  FIRST_NAME_REQUIRED: 'Error: First Name is required',
+  LAST_NAME_REQUIRED: 'Error: Last Name is required',
+  POSTAL_CODE_REQUIRED: 'Error: Postal Code is required',
+};
