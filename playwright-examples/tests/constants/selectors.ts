3fa0cc86a92c8b8a12b7f87a34048ebd0ab769b6
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-14 16:56:14 +0700

    feat: create constants for selectors

---
+++ playwright-examples/tests/constants/selectors.ts
@@ -0,0 +1,45 @@
+export const SELECTORS = {
+  // Common
+  TITLE: '.title',
+  CART_BADGE: '.shopping_cart_badge',
+  CART_ICON: '.shopping_cart_link',
+  INVENTORY_LIST: '.inventory_list',
+  INVENTORY_ITEM: '.inventory_item',
+  INVENTORY_ITEM_NAME: '.inventory_item_name',
+  INVENTORY_ITEM_PRICE: '.inventory_item_price',
+  INVENTORY_ITEM_DESC: '.inventory_item_desc',
+
+  // Login page
+  HEADING_LOGO: '.login_logo',
+  ERROR_ICON: '.error_icon',
+  ERROR_MESSAGE: '[data-test="error"]',
+  USERNAME: '[data-test="username"]',
+  PASSWORD: '[data-test="password"]',
+  LOGIN_BUTTON: '[data-test="login-button"]',
+
+  // Cart page
+  CART_ITEM: '.cart_item',
+
+  // Order/Checkout
+  FIRST_NAME: '[data-test="firstName"]',
+  LAST_NAME: '[data-test="lastName"]',
+  POSTAL_CODE: '[data-test="postalCode"]',
+  SUMMARY_SUBTOTAL: '.summary_subtotal_label',
+  SUMMARY_TAX: '.summary_tax_label',
+  SUMMARY_TOTAL: '.summary_total_label',
+  COMPLETE_HEADER: '.complete-header',
+  COMPLETE_TEXT: '.complete-text',
+  PONY_EXPRESS: '.pony_express',
+
+  // Sidebar
+  SIDEBAR_MENU: '.bm-menu',
+  MENU_BUTTON: '#react-burger-menu-btn',
+  CLOSE_BUTTON: '#react-burger-cross-btn',
+  ALL_ITEMS_LINK: '#inventory_sidebar_link',
+  ABOUT_LINK: '#about_sidebar_link',
+  LOGOUT_LINK: '#logout_sidebar_link',
+  RESET_APP_STATE_LINK: '#reset_sidebar_link',
+
+  // Sort
+  SORT_SELECT: '[data-test="product-sort-container"]',
+};
