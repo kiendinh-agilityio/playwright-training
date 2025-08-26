714de5bc784294018ac1e19f1c729cb0801f124f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-26 16:12:35 +0700

    feat: update auth setup

--- playwright-practices/dashboard-users/tests/auth.setup.ts
+++ playwright-practices/dashboard-users/tests/auth.setup.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { test } from '@/fixtures/pageFixtures';
 import { CREDENTIALS } from '@/constants';
 
-const STORAGE_STATE = path.join(__dirname, '.auth/user.json');
+const STORAGE_STATE = path.join(process.cwd(), 'playwright/.auth/user.json');
 
 test('authenticate', async ({ loginPage, dashboardPage, page }) => {
   const { username, password } = CREDENTIALS.ACCOUNT_SUCCESS;
