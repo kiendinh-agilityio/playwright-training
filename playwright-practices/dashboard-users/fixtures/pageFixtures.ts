f7ff70d95397c6b2c11a09166747eed78bc6b6d7
Kien Dinh <kien.dinh@asnet.com.vn>
2025-08-28 17:38:24 +0700

    feat: update pageFixtures

--- playwright-practices/dashboard-users/fixtures/pageFixtures.ts
+++ playwright-practices/dashboard-users/fixtures/pageFixtures.ts
@@ -1,10 +1,12 @@
 import { test as base, expect } from '@playwright/test';
 import { LoginPage, DashboardPage, UsersPage } from '@/pages';
+import { UserApiClient } from '@/services/UserApiClient';
 
 type Fixtures = {
   loginPage: LoginPage;
   dashboardPage: DashboardPage;
   usersPage: UsersPage;
+  userApi: UserApiClient;
 };
 
 export const test = base.extend<Fixtures>({
@@ -21,6 +23,10 @@ export const test = base.extend<Fixtures>({
   usersPage: async ({ page }, use) => {
     const users = new UsersPage(page);
     await use(users);
+  },
+  userApi: async ({ request }, use) => {
+    const client = new UserApiClient(request);
+    await use(client);
   },
 });
 
