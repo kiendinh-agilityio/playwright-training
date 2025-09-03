c844aaa85bf2a1995fd159aec3ce23132d5bd8b4
Kien Dinh <kien.dinh@asnet.com.vn>
2025-09-03 17:48:56 +0700

    feat: update naming for HttpMethod

--- playwright-practices/dashboard-users/utils/api.ts
+++ playwright-practices/dashboard-users/utils/api.ts
@@ -1,9 +1,9 @@
 import { API } from '@/constants';
 import { Page } from '@playwright/test';
 
-type HttpMethodLiteral = 'POST' | 'PUT' | 'PATCH' | 'GET';
+type HttpMethod = 'POST' | 'PUT' | 'PATCH' | 'GET';
 
-const USER_METHODS: Record<string, ReadonlyArray<HttpMethodLiteral>> = {
+const USER_METHODS: Record<string, ReadonlyArray<HttpMethod>> = {
   CREATE: ['POST'],
   EDIT: ['PATCH', 'PUT', 'POST'],
   GET: ['GET'],
@@ -11,7 +11,7 @@ const USER_METHODS: Record<string, ReadonlyArray<HttpMethodLiteral>> = {
 
 async function waitForUsersResponseByMethods(
   page: Page,
-  methods: ReadonlyArray<HttpMethodLiteral>,
+  methods: ReadonlyArray<HttpMethod>,
   urlContains?: string,
   returnJson: boolean = true,
 ) {
@@ -20,7 +20,7 @@ async function waitForUsersResponseByMethods(
       const responseUrl = res.url();
       const method = res.request().method();
       const isUsersCollection = responseUrl.includes(API.USERS);
-      const isDesiredMethod = methods.includes(method as HttpMethodLiteral);
+      const isDesiredMethod = methods.includes(method as HttpMethod);
       const hasUrlContains = urlContains ? responseUrl.includes(urlContains) : true;
       return isUsersCollection && isDesiredMethod && hasUrlContains;
     },
