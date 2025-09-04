d72d59256e02aa70383d1ada46ff980faf8b615f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-09-04 09:19:17 +0700

    feat: update constants users api

--- playwright-practices/dashboard-users/constants/api.ts
+++
@@ -1,5 +0,0 @@
-export const USERS_COLLECTION_ID = 'POWMOh0W6IoLUAI';
-
-export const API = {
-  USERS: `/api/collections/${USERS_COLLECTION_ID}/records`,
-};
---
+++ playwright-practices/dashboard-users/constants/user.ts
@@ -0,0 +1,3 @@
+export const USERS_COLLECTION_ID = 'POWMOh0W6IoLUAI';
+
+export const USERS_API = `/api/collections/${USERS_COLLECTION_ID}/records`;
