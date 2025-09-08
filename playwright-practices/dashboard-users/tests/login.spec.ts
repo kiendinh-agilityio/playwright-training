b04cdf5c1842d4f12e72877e1a88ec5cfd9d3a1e
Kien Dinh <kien.dinh@asnet.com.vn>
2025-09-08 14:28:02 +0700

    feat: update logic test

--- playwright-practices/dashboard-users/tests/login.spec.ts
+++ playwright-practices/dashboard-users/tests/login.spec.ts
@@ -1,6 +1,5 @@
 import { test } from '@/fixtures/pageFixtures';
 import { CREDENTIALS } from '@/constants';
-import { runLoginFlow } from '@/utils/login';
 
 test.use({ storageState: { cookies: [], origins: [] } });
 
@@ -13,26 +12,38 @@ test.describe('Login Functionality', () => {
   });
 
   test('Login successfully with valid credentials', async ({ loginPage, dashboardPage }) => {
-    await runLoginFlow(
-      loginPage,
-      dashboardPage,
-      CREDENTIALS.ACCOUNT_SUCCESS.username,
-      CREDENTIALS.ACCOUNT_SUCCESS.password,
-      'success',
-    );
+    await test.step('Fill email & password and click Login', async () => {
+      await loginPage.login(
+        CREDENTIALS.ACCOUNT_SUCCESS.username,
+        CREDENTIALS.ACCOUNT_SUCCESS.password,
+      );
+    });
+
+    await test.step('Verify user navigates to Users page', async () => {
+      await dashboardPage.assertUsersBreadcrumbVisible();
+    });
   });
 
-  test('Login failed with wrong email or password', async ({ loginPage, dashboardPage }) => {
-    await runLoginFlow(
-      loginPage,
-      dashboardPage,
-      CREDENTIALS.ACCOUNT_INVALID.username,
-      CREDENTIALS.ACCOUNT_INVALID.password,
-      'invalidCredentials',
-    );
+  test('Login failed with wrong email or password', async ({ loginPage }) => {
+    await test.step('Fill email & password and click Login', async () => {
+      await loginPage.login(
+        CREDENTIALS.ACCOUNT_INVALID.username,
+        CREDENTIALS.ACCOUNT_INVALID.password,
+      );
+    });
+
+    await test.step('Verify error toast is shown', async () => {
+      await loginPage.assertInvalidCredentials();
+    });
   });
 
-  test('Login failed with empty inputs', async ({ loginPage, dashboardPage }) => {
-    await runLoginFlow(loginPage, dashboardPage, '', '', 'emptyValidation');
+  test('Login failed with empty inputs', async ({ loginPage }) => {
+    await test.step('Fill email & password and click Login', async () => {
+      await loginPage.login('', '');
+    });
+
+    await test.step('Verify validation message is displayed', async () => {
+      await loginPage.assertEmptyFieldValidation();
+    });
   });
 });
